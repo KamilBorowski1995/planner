53907a60671c5677ced25f93bcb3694ab94ee634
Kamil Borowski <k.borowski@onet.pl>
2021-04-04 10:38:06 +0200

    created theme and initial template

---
+++ src/templates/MainTemplate.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+function MainTemplate() {
+  return (
+    <div>
+      <p>Witaj w templatce</p>
+    </div>
+  );
+}
+
+export default MainTemplate;
---
+++ src/theme/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    dark: {
+      primary: "#1F2128",
+      secondary: "#ECECED",
+      tertiary: "#768991",
+    },
+  },
+  font: {
+    primary: "'Roboto', sans-serif",
+    secondary: "'Inter', sans-serif",
+  },
+  size: {
+    xs: "1.024rem",
+    s: "1.28rem",
+    m: "1.6rem",
+    l: "2rem",
+    xl: "2.5rem",
+  },
+};
