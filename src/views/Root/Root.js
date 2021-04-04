842568864e7c419b3988e44ebf82ef794f827f01
Kamil Borowski <k.borowski@onet.pl>
2021-04-04 10:10:32 +0200

    initial refactor and install react router and styled component

---
+++ src/views/Root/Root.js
@@ -0,0 +1,11 @@
+import "./Root.css";
+
+function Root() {
+  return (
+    <div className="App">
+      <header className="App-header">Strona główna</header>
+    </div>
+  );
+}
+
+export default Root;
